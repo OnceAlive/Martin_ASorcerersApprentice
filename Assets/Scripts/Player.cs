9884d41c1381ef40b7a413156fddaa11a3b8e17a
Göbel, Tim <timgoebel148@gmail.com>
2023-04-14 09:29:15 +0200

    Fix Object Cast
    Player stops when trying to walk through another object

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,18 +13,19 @@ public class Player : MonoBehaviour
     {
         Vector2 inputVector = gameInput.GetMovementVector2Normalized();
         Vector3 move = new Vector3(inputVector.x, inputVector.y, 0f);       //convert to Vector3 because GameObject.transform cant handle with Vector2
-
+        
         float moveDistance = movementSpeed * Time.deltaTime;
+        Vector3 halfExtents = transform.lossyScale / 2;
 
         //checks if there is any object
-        bool canMove = !Physics.Raycast(transform.position, move, moveDistance);
-        Debug.Log(canMove);
+        bool canMove = !Physics.BoxCast(transform.position, halfExtents, move, new Quaternion(0,0,1,0), moveDistance);
+        Debug.Log("Can Move? : " + canMove);
 
         //if there is an object player should not move
         if(!canMove)
         {
             Vector2 moveX = new Vector2(move.x, 0f);
-            canMove = !Physics.Raycast(transform.position, moveX, moveDistance);
+            canMove = !Physics.BoxCast(transform.position, halfExtents, moveX);
 
             if(canMove)
             {
@@ -33,7 +34,7 @@ public class Player : MonoBehaviour
             else
             {
                 Vector2 moveY = new Vector2(0f, move.y);
-                canMove = !Physics.Raycast(transform.position, moveY, moveDistance);
+                canMove = !Physics.BoxCast(transform.position, halfExtents, moveY);
 
                 if(canMove)
                 {
