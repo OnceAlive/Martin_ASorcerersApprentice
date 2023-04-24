14ab30f8bc15ec8db699d461b8affa92d25e4ba7
Göbel, Tim <timgoebel148@gmail.com>
2023-04-24 13:54:45 +0200

    add Unity new Input System

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
