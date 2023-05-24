b691b0b48713ff2da7f0318bd8e3a2d26a6140a8
Göbel, Tim <timgoebel148@gmail.com>
2023-05-24 19:41:32 +0200

    add Sprites and refectoring of Player

--- Assets/Scripts/Combat/Flash.cs
+++ Assets/Scripts/Combat/Flash.cs
@@ -5,22 +5,21 @@ public class Flash : MonoBehaviour
 {
     [SerializeField] private Material flashMaterial;
     [SerializeField] private float restoreDefaultMaterialTime = .2f;
-
-    private SpriteRenderer spriteRenderer;
+    [SerializeField] private SpriteRenderer martinRenderer;
+    
     private Material defaultMaterial;
 
     public float GetRestoreDefaultMaterialTime => restoreDefaultMaterialTime;
 
     private void Awake()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        defaultMaterial = spriteRenderer.material;
+        defaultMaterial = martinRenderer.material;
     }
 
     public IEnumerator FlashRoutine()
     {
-        spriteRenderer.material = flashMaterial;
+        martinRenderer.material = flashMaterial;
         yield return new WaitForSeconds(restoreDefaultMaterialTime);
-        spriteRenderer.material = defaultMaterial;
+        martinRenderer.material = defaultMaterial;
     }
 }
